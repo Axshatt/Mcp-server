5ff36462f848cf507e7711ffb6807e342a8758a4
Akshat <codepreneur.in@gmail.com>
2025-08-05 22:03:50 +0530

    added gemini iniegrity

--- client/index.js
+++ client/index.js
@@ -1,37 +1,47 @@
-const readline= require('readline/promises')
-const  {GoogleGenAI}  = require('@google/genai') 
+const readline = require('readline/promises')
+const { GoogleGenAI } = require('@google/genai')
 require('dotenv').config();
 
-const ai = new GoogleGenAI({apiKey:process.env.GEMINI_API_KEY});
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 const chatHistory = [];
 
 const rl = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout,   
+    input: process.stdin,
+    output: process.stdout,
 })
 
 
-async function chatLoop(){
-    
-    const responseText = response.candidates[0].content.parts[0].text;
+async function chatLoop() {
+
+    const question = await rl.question("You: ")
     chatHistory.push({
-        role:"user",
-        parts:[
+        role: "user",
+        parts: [
             {
-                text:responseText,
-                type:"text"
+                text: question,
+                type: "text"
             }
         ]
     })
 
     const response = await ai.models.generateContent({
-        model:"gemini-2.5-flash",
-        contents:chatHistory
+        model: "gemini-2.5-flash",
+        contents: chatHistory
 
-    }) 
-    console.log("AI: "+responseText);
+    })
+    const responseText = response.candidates[0].content.parts[0].text;
+    chatHistory.push({
+        role: "user",
+        parts: [
+            {
+                text: responseText,
+                type: "text"
+            }
+        ]
+    })
+    console.log("AI: " + responseText);
     chatLoop()
-    
+
 }
 chatLoop()
